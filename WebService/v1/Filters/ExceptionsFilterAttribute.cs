598a45b4ba1b02be2d337709989e49aa1eb9c13a
Devis Lucato <dluc@users.noreply.github.com>
2018-07-06 18:17:03 -0700

    bugfix: support scenario where unexpected exceptions don't have a stack trace

--- WebService/v1/Filters/ExceptionsFilterAttribute.cs
+++ WebService/v1/Filters/ExceptionsFilterAttribute.cs
@@ -94,14 +94,14 @@ namespace Microsoft.Azure.IoTSolutions.StorageAdapter.WebService.v1.Filters
 
             if (stackTrace)
             {
-                error["StackTrace"] = e.StackTrace.Split(new[] { "\n" }, StringSplitOptions.None);
+                error["StackTrace"] = e.StackTrace?.Split(new[] { "\n" }, StringSplitOptions.None);
 
                 if (e.InnerException != null)
                 {
                     e = e.InnerException;
                     error["InnerExceptionMessage"] = e.Message;
                     error["InnerExceptionType"] = e.GetType().FullName;
-                    error["InnerExceptionStackTrace"] = e.StackTrace.Split(new[] { "\n" }, StringSplitOptions.None);
+                    error["InnerExceptionStackTrace"] = e.StackTrace?.Split(new[] { "\n" }, StringSplitOptions.None);
                 }
             }
 
